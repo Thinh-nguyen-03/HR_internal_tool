92e9aff11c3a53b627c1f0658e512ff553997c88
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2026-01-28 16:30:52 -0600

    Update surveys_fetch.py

--- surveys_fetch.py
+++ surveys_fetch.py
@@ -7,7 +7,7 @@ import requests
 from io import StringIO
 from datetime import datetime
 from typing import List, Dict, Any, Optional
-from cultureindex_client_1 import CultureIndexClient, CultureIndexAuthError
+from cultureindex_client import CultureIndexClient, CultureIndexAuthError
 
 try:
     import phonenumbers
@@ -216,3 +216,4 @@ def fetch_all_surveys():
 
 if __name__ == "__main__":
     sys.exit(fetch_all_surveys())
+
